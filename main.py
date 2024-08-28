4ae689cd03d53ef3dd30c03befaeb64b4c0d9b31
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2024-08-28 14:27:35 +0900

    포트폴리오 주소 업데이트

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ markdown_text = """
 
 [![Solved.ac Profile](http://mazassumnida.wtf/api/v2/generate_badge?boj=tae77777)](https://solved.ac/tae77777/)
 
-## 🌟 <a href="https://tae77777.oopy.io/" style="color:#FF6347; text-decoration:none; font-weight:bold;" onmouseover="this.style.color='#FFD700'; this.style.transition='0.3s';" onmouseout="this.style.color='#FF6347';">My Portfolio</a> 🌟
+## 🌟 [**My Portfolio (https://tae77777.oopy.io/)**](https://tae77777.oopy.io/) 🌟
 
 🔗 **<span style="color:#FF6347;">Check out my work and projects here!</span>**
 
